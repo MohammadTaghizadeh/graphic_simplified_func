931bb61ac2ef39a0456d4d8fa6e0c444caf21593
mtagizadehbioinf <mtaghizadeh@alumni.ut.ac.ir>
2023-02-26 14:39:14 +0330

    correct scatter

--- R/main.R
+++ R/main.R
@@ -21,9 +21,11 @@ mtbarplot <- function(data, bar_lab, y_lab = NULL, x_lab = NULL,
 
 ################################################################################
 
-mtscatterplot <- function(df, plot_name = NULL, x_lab = NULL, y_lab = NULL){
+mtscatterplot1 <- function(data_1, data_2,
+                          plot_name = NULL, x_lab = NULL, y_lab = NULL){
   library(ggplot2)
-  ggplot(df, aes(lstat, medv)) +
+  df <- data.frame(data1 = data_1, data2 = data_2)
+  ggplot(df, aes(data1, data2)) +
     geom_point(alpha = 0.4, size = 3) + theme_bw() +
     theme(panel.background = element_rect(fill = 'white', color = 'black',
                                           size = 1.4),
@@ -34,8 +36,9 @@ mtscatterplot <- function(df, plot_name = NULL, x_lab = NULL, y_lab = NULL){
           axis.text.y = element_text(size = 12, face = 'bold'),
           axis.title = element_text(size = 14, face = 'bold'),
           plot.title = element_text(size = 14, face = 'bold', hjust = 0.5)) +
-    scale_x_continuous(breaks = seq(0, 45, 3)) +
-    scale_y_continuous(breaks = seq(0, 50, 5)) +
+    scale_x_continuous(breaks = seq(min(data_1), max(data_1)+1, 4)) +
+    scale_y_continuous(breaks = seq(min(data_2), max(data_2)+1, 5)) +
+
     labs(title = plot_name, x = x_lab, y = y_lab)
 
 }
@@ -49,3 +52,26 @@ remove_gsf <- function(){
 install_gsf <- function(){
   devtools::install_github('MohammadTaghizadeh/graphic_simplified_func@master')
 }
+
+
+library(idpr)
+hsa <- unlist(protr::getUniProt('P02768'))
+hsa
+
+scaledHydropathyLocal(hsa,
+                      plotResults = TRUE)
+
+ex_globin <- unlist(protr::getUniProt('P28316'))
+
+scaledHydropathyLocal(ex_globin,
+                      plotResults = TRUE)
+
+foldIndexR(sequence = hsa,
+           plotResults = TRUE)
+
+
+foldIndexR(sequence = ex_globin,
+           plotResults = TRUE)
+
+
+mtscatterplot1(cars$speed, cars$dist)
